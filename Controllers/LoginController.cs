e25453a45d48394ec580b51868617d169b8bbb5d
Panupon <baspanupon84862@gmail.com>
2024-07-11 20:24:15 +0700

    first commit

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication_bas.Models;
+
+public class LoginController : Controller
+{
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [Route("/Login")]
+    public IActionResult Login(LoginViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            // ตรวจสอบข้อมูลจากฐานข้อมูลหรือระบบการยืนยันตัวตน
+            // ในที่นี้จะเป็นตัวอย่างเท่านั้น
+
+            if (model.Email == "user@example.com" && model.Password == "password")
+            {
+                // ในที่นี้เราจะให้เข้าสู่ระบบเสมอ
+                return RedirectToAction("Login", "Profile"); // หรือไปยังหน้าที่ต้องการหลังจาก Login สำเร็จ
+            }
+            else
+            {
+                // กรณีที่ข้อมูลไม่ถูกต้อง
+                ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+            }
+        }
+        return View(model);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "Login",
+    pattern: "{controller=Home}/{action=index}/{id?}");
+
+app.Run();
